f370d969a271da76949c84334adbacbf59080143
Alejo <aljiro@gmail.com>
2019-12-12 09:11:24 +0000

    final version2

--- kbandit.py
+++ kbandit.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
 
 import rospy
-from std_msgs.msg import Float32MultiArray, UInt32MultiArray, UInt16MultiArray, UInt8MultiArray, UInt16, Int16MultiArray, String
+import miro2 as miro
+import os
+
+from std_msgs.msg import Float32MultiArray, UInt32MultiArray
 from geometry_msgs.msg import TwistStamped
-from sensor_msgs.msg import JointState, BatteryState, Imu, Range, CompressedImage
 
 import numpy as np
 import time
-import miro2 as miro
-import os
-import math
 
+
+# Constants
 MAX_TIME = 10.0
-
-#Generate a fake enum for joint arrays
+# Utility enums
 tilt, lift, yaw, pitch = range(4)
 droop, wag, left_eye, right_eye, left_ear, right_ear = range(6)
-freq, volume, duration = range(3)
-front_left, mid_left, rear_left, front_right, mid_right, rear_right = range(6)
 
 class MiroController:
 
 	def __init__( self ):
 		print "Initializing the controller"
-		self.actions = [ self.earWiggle, self.tailWag, self.rotate, self.nod ]
+		self.actions = [ self.earWiggle, self.tailWag, self.rotate, self.shine ]
+		self.Q = [0]*4
+		self.N = [0]*4
+		self.r = 0
 
 		# Set robot name
 		topic_root = "/" + os.getenv("MIRO_ROBOT_NAME")
@@ -33,13 +34,12 @@ class MiroController:
 		self.pub_cmd_vel = rospy.Publisher(topic_root + "/control/cmd_vel", TwistStamped, queue_size=0)
 		self.pub_cos = rospy.Publisher(topic_root + "/control/cosmetic_joints", Float32MultiArray, queue_size=0)
 		self.pub_illum = rospy.Publisher(topic_root + "/control/illum", UInt32MultiArray, queue_size=0)
-		self.pub_kin = rospy.Publisher(topic_root + "/control/kinematic_joints", JointState, queue_size=0)
+
+		# Subscribers
+		rospy.Subscriber(topic_root + '/sensors/package', miro.msg.sensors_package, self.touchListener)
 
 		# Initializing object for data publishing
 		self.velocity = TwistStamped()
-		self.kin_joints = JointState()
-		self.kin_joints.name = ["tilt", "lift", "yaw", "pitch"]
-		self.kin_joints.position = [0.0, math.radians(34.0), 0.0, 0.0]
 		self.cos_joints = Float32MultiArray()
 		self.cos_joints.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 		self.illum = UInt32MultiArray()
@@ -48,37 +48,45 @@ class MiroController:
 
 	# Actions
 	def earWiggle( self, t ):
-		A = 1.5
+		A = 1.0
 		w = 2*np.pi*0.2
 		f = lambda t: A*np.cos(w*t)
 
-		self.cos_joints.data[left_ear] = f(t)
-		self.cos_joints.data[right_ear] = f(t)
-		self.pub_cos.publish(self.cos_joints)
+		if t > MAX_TIME:
+			self.cos_joints.data[left_ear] = 0.0
+			self.cos_joints.data[right_ear] = 0.0
+			r = False
+		else:
+			self.cos_joints.data[left_ear] = f(t)
+			self.cos_joints.data[right_ear] = f(t)
+			r = True
 
+		
+		self.pub_cos.publish(self.cos_joints)
 		print "wiggle"
 
-		if t > MAX_TIME:
-			return False
-		else:
-			return True
+		return r
+		
 
 	def tailWag( self, t ):
-		A = 1.5
+		A = 1.0
 		w = 2*np.pi*0.2
 		f = lambda t: A*np.cos(w*t)
-		self.cos_joints.data[wag] = f(t)
-		self.cos_joints.data[droop] = 0.0
-
-		self.pub_cos.publish(self.cos_joints)
-
-		print "wag"
 
 		if t > MAX_TIME:
-			return False
+			self.cos_joints.data[wag] = 0.5
+			r = False
 		else:
-			return True
+			self.cos_joints.data[wag] = f(t)
+			r = True
 
+		self.cos_joints.data[droop] = 0.0
+		self.pub_cos.publish(self.cos_joints)
+		print "wag"
+
+		return r
+
+		
 	def rotate( self, t ):
 
 		if t > MAX_TIME:
@@ -93,42 +101,74 @@ class MiroController:
 		wheel_speed = [l_val, r_val]
 		(dr, dtheta) = miro.utils.wheel_speed2cmd_vel(wheel_speed)
 		self.velocity.twist.linear.x = dr
-		self.velocity.twist.angular.z = dtheta
-
+		self.velocity.twist.angular.z = dtheta		
 		self.pub_cmd_vel.publish(self.velocity)
-
 		print "rotate"
 
 		return r
 
 
-	def nod( self, t ):
-		print "nod"
+	def shine( self, t ):
+		color = 0xFF00FF00
+		f = lambda t: t*30/MAX_TIME
+		ip = int(np.mod(np.rint(f(t)), 6))
+		i = int(np.mod(ip + 1, 6))
+		self.illum.data[ip] = 0x00000000
 
-		return False
+		if t > MAX_TIME:
+			self.illum.data[i] = 0x00000000			
+			r = False
+		else:
+			self.illum.data[i] = color			
+			r = True
+
+		self.pub_illum.publish(self.illum)
+		print "Shinnning"
+
+		return r
 
 	# Listeners
-	def touchListener( self ):
-		pass
+	def touchListener( self, data ):
+		touch_data = data.touch_body.data
+		strength = 0
+
+		for i in range(0, 14):
+			if ((1 << i) & touch_data) > 0:
+				strength += 1
+
+		if strength > 2:
+			reward = 1.0
+		else:
+			reward = 0.0
+
+		print touch_data
+
+		self.Q[self.r] += (reward - self.Q[self.r])/float(self.N[self.r])
 
 	# Main loop
 	def run( self ):
 		running = True
-		r = np.random.randint( 0, len(self.actions) )
+		self.r = np.random.randint( 0, len(self.actions) )
+		self.N[self.r] += 1
 		t = 0.0
 		h = 0.1
 
 		print "Starting the loop"
 
 		while( running ):
-			if not self.actions[r]( t ):
+			if not self.actions[self.r]( t ):
 				print "Action Finished, changing action"
 				# Action selection
-				r = np.random.randint( 0, len(self.actions) )
+				if np.random.random < 0.5:	
+					self.r = np.random.randint( 0, len(self.actions) )
+				else:
+					self.r = np.argmax( self.Q )
+						
+				self.N[self.r] += 1
 				t = 0.0
 			
 			t = t + h			
-			time.sleep(0.01)
+			time.sleep(0.05)
 
 if __name__ == "__main__":
 	mc = MiroController()
